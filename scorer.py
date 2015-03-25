2237c9ac7f51e9697424d738eb3f0951fca7bf3c
Anubhav <anubhav1691@gmail.com>
2015-03-25 13:14:47 +0530

    Fixed the level of indentation
    According to the linux coding guidelines, I would
    suggest to keep the indentation level consistent
    with 4 tab spaces. Please try to send the future
    commits with 4 tab space indentation level.

--- scorer.py
+++ scorer.py
@@ -7,10 +7,10 @@ from time import sleep
 
 
 def sendmessage(title, message):
-  Notify.init("Scorer")
-  scorer = Notify.Notification.new(title, message, "dialog-information")
-  scorer.show()
-  return
+    Notify.init("Scorer")
+    scorer = Notify.Notification.new(title, message, "dialog-information")
+    scorer.show()
+    return
 
 
 url = "http://static.cricinfo.com/rss/livescores.xml"
@@ -20,32 +20,32 @@ interrupted=False
 
 print("Fetching matches..")
 while True:
-  try:
-    r = requests.get(url)
-    while r.status_code is not 200:
-      sleep(2)
-      r = requests.get(url)
-    soup = BeautifulSoup(r.text)
-    data = soup.find_all("description")
-    if match == 0:
-      print("Matches available:")
-      counter = 1
-      for game in data[1:]:
-        print(counter, game.text)
-        counter += 1
-      match = int(input("Enter your choice: "))
-      interrupted=False
-    newscore = data[match].text
-    if newscore != score:
-      score = newscore
-      sendmessage("Score", score)
-    sleep(15)
+    try:
+        r = requests.get(url)
+        while r.status_code is not 200:
+            sleep(2)
+            r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        data = soup.find_all("description")
+        if match == 0:
+            print("Matches available:")
+            counter = 1
+            for game in data[1:]:
+                print(counter, game.text)
+                counter += 1
+            match = int(input("Enter your choice: "))
+            interrupted=False
+        newscore = data[match].text
+        if newscore != score:
+            score = newscore
+            sendmessage("Score", score)
+        sleep(15)
 
-  except KeyboardInterrupt:
-    if interrupted:
-      print("Bye bye")
-      break
-    else:
-      print("Press Ctrl+C again to quit")
-      match = 0
-      interrupted=True
+    except KeyboardInterrupt:
+        if interrupted:
+            print("Bye bye")
+            break
+        else:
+            print("Press Ctrl+C again to quit")
+            match = 0
+            interrupted=True
