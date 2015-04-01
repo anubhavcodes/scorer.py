59d2c4f187769069c029fe97554088a297fb022d
Anubhav <anubhav1691@gmail.com>
2015-04-01 23:58:04 +0530

    Added absolute imports to app.py and used main()
    I have used absolute imports now. For eg:
    from scorer.system import exitApp.
    
    I have also added the main function and the used
    __main__.py which now acts as the starting point for
    the script.
    
    This changes the way how the script will run. You
    SHOULD NOT run ``python scorer/app.py `` to run this
    script. Insted you should run ``python -m scorer`` from the
    root of the project.
    
    My next step would be to use either distutils/setuptools
    to package this app properly!
    
    And then I will merge this into the master branch

---
+++ scorer/__main__.py
@@ -0,0 +1,2 @@
+from scorer.app import main
+main()
--- scorer/app.py
+++ scorer/app.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-from system import exitApp
-import fetch_scores as fs
-import notification as notify
+from scorer.system import exitApp
+import scorer.fetch_scores as fs
+import scorer.notification as notify
 import logging
 from time import sleep
 
@@ -10,45 +10,49 @@ logging.basicConfig(level=logging.DEBUG)
 NO_LIVE_MATCHES = "No Match in progress"
 SLEEP_INTERVAL = 15 
 
-while True:
-    logging.debug("Getting the xml and matches list")
-    xml, matches = fs.findMatchesAvailable()
-    if(matches[0]==NO_LIVE_MATCHES):
-        print("No Live matches are available now:")
-        exitApp()
-    print("The following matches are available now:")
-    for index,game in enumerate(matches, 1):
-        print (index, ".", game)
-    print (index+1, ". Quit ")
-    try:
-        matchChoice = str(input("Enter your choice: ")).strip()
-    except KeyboardInterrupt:
-            exitApp()   
+def main():
     while True:
-        if (matchChoice.isdigit()):
-            matchChoice = int(matchChoice)
-            if (matchChoice==index+1):
-                logging.info("User selected Quit")
-                exitApp()
-            if matchChoice in range(1, index+1):
+        logging.debug("Getting the xml and matches list")
+        xml, matches = fs.findMatchesAvailable()
+        if(matches[0]==NO_LIVE_MATCHES):
+            print("No Live matches are available now:")
+            exitApp()
+        print("The following matches are available now:")
+        for index,game in enumerate(matches, 1):
+            print (index, ".", game)
+        print (index+1, ". Quit ")
+        try:
+            matchChoice = str(input("Enter your choice: ")).strip()
+        except KeyboardInterrupt:
+                exitApp()   
+        while True:
+            if (matchChoice.isdigit()):
+                matchChoice = int(matchChoice)
+                if (matchChoice==index+1):
+                    logging.info("User selected Quit")
+                    exitApp()
+                if matchChoice in range(1, index+1):
+                    break
+            logging.debug("User's choice was invalid")
+            try:
+                matchChoice = raw_input("Enter your choice: ").strip()
+            except KeyboardInterrupt:
+                exitApp()  
+        #logging moved down after validation since matches[matchChoice-1] could lead to exception 
+        #for unvalidated match choices.
+        logging.info("User's choice: {} {}".format(matchChoice, matches[matchChoice-1]))
+        logging.info("Getting the latest score for the selected match")
+        matchID = fs.getMatchID(matchChoice-1, xml)
+        jsonurl = fs.getJsonURL(matchID)
+        playingTeams = fs.getPlayingTeamNames(jsonurl)
+        while True:
+            try:
+                title,score = fs.getLastestScore(jsonurl,playingTeams)
+                logging.debug("Sending notification for: title:{} score:{}".format(title, score))
+                notify.popUpMessage(title, score)
+                sleep(SLEEP_INTERVAL)
+            except KeyboardInterrupt:
                 break
-        logging.debug("User's choice was invalid")
-        try:
-            matchChoice = raw_input("Enter your choice: ").strip()
-        except KeyboardInterrupt:
-            exitApp()  
-    #logging moved down after validation since matches[matchChoice-1] could lead to exception 
-    #for unvalidated match choices.
-    logging.info("User's choice: {} {}".format(matchChoice, matches[matchChoice-1]))
-    logging.info("Getting the latest score for the selected match")
-    matchID = fs.getMatchID(matchChoice-1, xml)
-    jsonurl = fs.getJsonURL(matchID)
-    playingTeams = fs.getPlayingTeamNames(jsonurl)
-    while True:
-        try:
-            title,score = fs.getLastestScore(jsonurl,playingTeams)
-            logging.debug("Sending notification for: title:{} score:{}".format(title, score))
-            notify.popUpMessage(title, score)
-            sleep(SLEEP_INTERVAL)
-        except KeyboardInterrupt:
-            break
+
+if __name__ == '__main__':
+    main()
