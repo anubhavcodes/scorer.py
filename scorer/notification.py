ae62f3096f8d8f2269511952e88487f062d8451a
Anubhav <anubhav1691@gmail.com>
2015-04-17 13:29:04 +0530

    Better handling for notification module
    There were instances when pynotify failed to send the notification
    to dbus maybe, and hence there was a gio.Error thrown.
    
    Although the error itself is yet to be debugged, this commit handles the
    error and quits the app gracefully. This fixes #37

--- scorer/notification.py
+++ scorer/notification.py
@@ -4,5 +4,12 @@ import pynotify
 
 def popUpMessage(title, message):
     logging.debug("Initializing pynotify")
-    pynotify.init("Scorer")
-    pynotify.Notification(title, message, "dialog-information").show()
+    try:
+        pynotify.init("Scorer")
+        pynotify.Notification(title, message, "dialog-information").show()
+    except Exception as e:
+        logging.debug("Error initializing pynotify")
+        logging.debug(e)
+        logging.info("Unable to initialize pynotify: Connection Refused")
+        logging.info("Quitting the app")
+        exit()
