ad282bf976bb6fd45b6598eec62a9bc3c4a3a14b
Anubhav <anubhav1691@gmail.com>
2015-07-03 00:39:39 +0530

    Removed dependency on pynotify
    Many users were facing problem with different notify packages for
    their own distribution, and it was an headache supporting every one
    of them in the code.
    
    Calling notify-send as a subprocess makes life much much simpler
    now.
    
    However I think there can be a better implementation for the
    subprocess call then just using subprocess.call(command)
    
    I will work on the next commmit soon

--- scorer/notification.py
+++ scorer/notification.py
@@ -1,18 +1,14 @@
 from sys import version_info
 from scorer.system import exitApp
 import logging
-import pynotify 
+import subprocess as sp
 
 logger = logging.getLogger('scorer.notification')
 
 def popUpMessage(title, message):
-    logger.info("Initializing pynotify")
     try:
-        pynotify.init("Scorer")
-        pynotify.Notification(title, message, "dialog-information").show()
+        command = ['notify-send', title, message]
+        pipe = sp.call(command)
     except Exception as e:
-        logger.error("Error initializing pynotify")
         logger.debug(e)
-        logger.info("Unable to initialize pynotify: Connection Refused")
-        logger.info("Quitting the app")
         exitApp()
