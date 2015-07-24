5dfef85aebad8cbd5d8d82597227cccdde10efe8
Tushar Gautam <tushar.rishav@gmail.com>
2015-07-25 03:17:52 +0530

    PEP8 confirmations

--- scorer/app.py
+++ scorer/app.py
@@ -13,39 +13,43 @@ fh = logging.FileHandler("scorer.log")
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -\
+ %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
 NO_LIVE_MATCHES = "No Match in progress"
-SLEEP_INTERVAL = 60 
+SLEEP_INTERVAL = 60
+
 
 def main():
     while True:
         logger.debug("Getting the xml and matches list")
         xml, matches = fs.findMatchesAvailable()
-        if(matches[0]==NO_LIVE_MATCHES):
+        if(matches[0] == NO_LIVE_MATCHES):
             print "No Live matches are available now:"
             exitApp()
         matches.append("Quit the scorer app")
         try:
-            matchChoice= getUserInput(matches)
+            matchChoice = getUserInput(matches)
         except KeyboardInterrupt:
             exitApp()
-        if(matchChoice == len(matches) -1 ):
+        if(matchChoice == len(matches) - 1):
             logger.debug("User chose quit")
             exitApp()
-        logger.debug("User's choice: {} {}".format(matchChoice, matches[matchChoice-1]))
+        logger.debug("User's choice: {} {}".format(matchChoice,
+                                                   matches[matchChoice-1]))
         logger.debug("Getting the latest score for the selected match")
-        matchID = fs.getMatchID(matchChoice,xml)
+        matchID = fs.getMatchID(matchChoice, xml)
         jsonurl = fs.getJsonURL(matchID)
         playingTeams = fs.getPlayingTeamNames(jsonurl)
         while True:
             try:
-                title,score = fs.getLastestScore(jsonurl,playingTeams)
-                logger.debug("Sending notification for: title:{} score:{}".format(title, score))
+                title, score = fs.getLastestScore(jsonurl, playingTeams)
+                logger.debug("Sending notification for: title:{} score:\
+                    {}".format(title, score))
                 notify.popUpMessage(title, score)
                 sleep(SLEEP_INTERVAL)
             except KeyboardInterrupt:
