3a4c0e8a54d3bdbdad52ad0a474bb9027d2a42b2
Anubhav Yadav <anubhav1691@gmail.com>
2018-08-27 20:16:01 +0530

    Add `pynotify` to dependencies

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
 setup(
-    name = 'scorer',
-    version = 0.1,
-    description = 'A simple script to show desktop notifications for cricket scores',
-    long_description = readme(),
-    url = 'https://github.com/neo1691/scorer.py',
-    author = 'Anubhav Yadav', 
-    author_email = 'anubhav1691@gmail.com',
-    license = 'GPLv2',
-    packages = ['scorer'],
+    name='scorer',
+    version=0.1,
+    description='A simple script to show desktop notifications for cricket scores',
+    long_description=readme(),
+    url='https://github.com/neo1691/scorer.py',
+    author='Anubhav Yadav',
+    author_email='anubhav1691@gmail.com',
+    license='GPLv2',
+    packages=['scorer'],
     install_requires=[
-          'requests',
-          'beautifulsoup4',
-      ],
+        'requests',
+        'beautifulsoup4',
+        'pynotify'
+    ],
     entry_points={
-        'console_scripts':[
+        'console_scripts': [
             'scorer = scorer.app:main'
-            ]
-        },
-    zip_safe = False
-        )
+        ]
+    },
+    zip_safe=False
+)
